2c14e6d61374e4a1707f37424fe5a552986b3d89
bdavies03 <161866579+bdavies03@users.noreply.github.com>
2024-03-02 03:10:27 +0000

    Add files via upload

---
+++ double_words.py
@@ -0,0 +1,24 @@
+#A function that will filter out any words comprised of a string repeated twice from a list (words) and outputting them in a seperate list
+
+def filter_doubled(words):
+    i = 0
+    list_length = len(words)
+    output = []
+    while i < list_length:
+        words[i] = words[i].lower()
+        word_length = len(words[i])
+        count = 0
+        if word_length%2 == 0:
+            j = 0
+            while j < word_length/2:
+                if words[i][j] == words[i][j+int(word_length/2)]:
+                    count +=1
+                j += 1
+            if count == word_length/2:
+              output.append(words[i])
+        i += 1
+    return(output)
+
+
+words = ['bonbon', 'hello', 'pompom', 'hi', 'chicken', 'couscous', 'a', 'aa', 'aaa']
+print(filter_doubled(words))
